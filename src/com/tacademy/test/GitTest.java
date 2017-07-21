23bf95d894b316161e6b1aea690fa8a70c73b7d0
so7247 <so7247@naver.com>
2017-07-21 11:40:28 +0900

    branch1Test

--- src/com/tacademy/test/GitTest.java
+++ src/com/tacademy/test/GitTest.java
@@ -3,12 +3,20 @@ package com.tacademy.test;
 public class GitTest {
 
 	private String master;
-
+	private String branch1;
 	
 	
 	@Override
 	public String toString() {
-		return "GitTest [master=" + master + "]";
+		return "GitTest [master=" + master + ", branch1=" + branch1 + "]";
+	}
+
+	public String getBranch1() {
+		return branch1;
+	}
+
+	public void setBranch1(String branch1) {
+		this.branch1 = branch1;
 	}
 
 	public String getMaster() {
