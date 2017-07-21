0c118c61940bc763422e9cb6b8163295051b7a88
so7247 <so7247@naver.com>
2017-07-21 11:38:00 +0900

    Master_Start

---
+++ src/com/tacademy/test/GitTest.java
@@ -0,0 +1,23 @@
+package com.tacademy.test;
+
+public class GitTest {
+
+	private String master;
+
+	
+	
+	@Override
+	public String toString() {
+		return "GitTest [master=" + master + "]";
+	}
+
+	public String getMaster() {
+		return master;
+	}
+
+	public void setMaster(String master) {
+		this.master = master;
+	}
+	
+	
+}
