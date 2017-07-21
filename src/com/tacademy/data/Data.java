c1ea0c05e824a78e013567fd8b12fa4e0f62c202
so7247 <so7247@naver.com>
2017-07-21 09:31:16 +0900

    으아니??

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -2,10 +2,19 @@ package com.tacademy.data;
 
 public class Data {
 	private String name;
+	private int age;
+	
 
 	@Override
 	public String toString() {
-		return "Data [name=" + name + "]";
+		return "Data [name=" + name + ", age=" + age + "]";
+	}
+
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public String getName() {
