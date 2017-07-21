90851eb6823c8398001346f7592fb64800277758
so7247 <so7247@naver.com>
2017-07-21 09:26:41 +0900

    으아니!!

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -1,5 +1,19 @@
 package com.tacademy.data;
 
 public class Data {
+	private String name;
 
+	@Override
+	public String toString() {
+		return "Data [name=" + name + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
